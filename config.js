4f6da4d44f80647bf53f88f01876724737ce89f4
u3677 <115551223+u3677@users.noreply.github.com>
2024-06-30 07:50:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaDK8ZUDjiOh
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/1f4e6e1cc2ed1662adb36.jpg";
 global.devs = "923135673658";
-global.sudo = process.env.SUDO || "923192173398";
-global.owner = process.env.OWNER_NUMBER || "923192173398";
+global.sudo = process.env.SUDO || "22362451835";
+global.owner = process.env.OWNER_NUMBER || "22362451835";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -56,7 +56,7 @@ global.scan = "https://pair-qr-wasi-md.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV085VkZvZkQ2bkpkRTFQUHJoK1FMbHE4ZFYwUFBnRXZ2TXA1empPcGJsdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSGQvVDBYYVhuUU95YzVkT0Q4S3VyZHJJYTlWNEl6NHJIbjNkRnVRcG1CWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJQ3ZyQzhJbmJwYTkySjhUVjY3RlNVTjJ5VTVjV2w3VmpNUUJ2bDZSdUdzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTcEQ1QkZLMzM3bGh1c2xSV1RhWjcvbjBYcklsaFpoWGRpMDlFMzZZV2lZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlNLcjNUcXlTbkh5ODFZMndZVUFOOUF3MTVOclkrVGtBR2QxT2hEdXFKbFE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkVXR05XM2h4NmppSGhNWGVSMVdQcGt6bHU1NFQveTBEelR1Q3I0N2VpQk09In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ1BIcXk3K2w2bVQySk9KNENsNTJ2SGZCT1ZrdFAyT2FzOUx1VVNOQmtXVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiLzdnTEFiZ0x4VVBUMG1OZHBqclBYSENxdkxOTzQyK1B6b3JDVm16U0h4MD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IktibGVlQVRsNEgyMEhEQWlBZUtzckwrRk9YVFhLWWZGcDNrbXkwYjEwVGNmTFY3TTlJek93STBnSFBZVFZteTRPWTNwam8yQm1NL2YzaXBIUDNORkJ3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjMzLCJhZHZTZWNyZXRLZXkiOiJzbTFydHlmTHg2NUZMRzAySHdSNThHRGY2OXFxc3JhUUhvMS9DQzFOSytjPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJTNDRzT3JfLVJ5R2oyTWRFeS1fTGpRIiwicGhvbmVJZCI6ImEyYTg1ZTM1LTZmOGQtNDE1Zi1hZTQxLWRhMjM0ZGM2MzI2ZiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsQkw5ZFpQbVVmclR0M2xUS1dldXFsSUo2Vnc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSnJOdTA1WGlYUGczN3YrZ1orV0tQMTB1bm9vPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ik0zUlRUQjFDIiwibWUiOnsiaWQiOiIyMjM2MjQ1MTgzNToyQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNMUHMrQTBRbDUyQ3RBWVlBU0FBS0FBPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJ1R1VmSnBhNk9qU080d3hRdzVyK2l0R0VzRkFMZWF2T0Ywak9kaVpBZWl3PSIsImFjY291bnRTaWduYXR1cmUiOiJnZ28zNkJXcWh2bjA4QVJjeDRxVC93bDYwLzhtampTV1AvejlwaWFJdUMvV1VKcjJnRzQ3M1hWcldLU1FrUlBQMUd3NHRtNDVCT1BqL3VYOWJ0SDREZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiSy8wQzY3Q01wZGZFUUtVZ2huUTM2THZCZEJGQ2NxcnUvUHI5aVFiOGZLaUdvc2g4ZWNXRGwwN2tydGVKdTJZeG5hdHhETm5RbXdwRjBDUUtETWtKQ1E9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMjM2MjQ1MTgzNToyQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmJobEh5YVd1am8wanVNTVVNT2Evb3JSaExCUUMzbXJ6aGRJem5ZbVFIb3MifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MTk3MDExNTZ9"
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
@@ -65,8 +65,8 @@ module.exports = {
   caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘-𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2😍",
   author: process.env.PACK_AUTHER || "WASO",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2",
-  ownername: process.env.OWNER_NAME || "WASI",
+  botname: process.env.BOT_NAME || "✰⃞𝕜𝕒𝕜𝕦𝕫𝕦🅇⃠⁩☬☬⃢⃟𝙃𝙞𝙙𝙖𝙣",
+  ownername: process.env.OWNER_NAME || "✰⃞𝕜𝕒𝕜𝕦𝕫𝕦🅇⃠⁩☬☬⃢⃟𝙃𝙞𝙙𝙖𝙣",
   errorChat: process.env.ERROR_CHAT || "923192173398",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -77,7 +77,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
   AUTOREACTION: process.env.AUTOREACTION || process.env.AUTOREACTION || "true",
   LANG: (process.env.THEME || "WASI").toUpperCase(),
 };
